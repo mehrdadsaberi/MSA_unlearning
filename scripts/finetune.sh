ef767a8be1b8f02978acb24ab64edc6c6450b300
Mehrdad Saberi <77437101+mehrdadsaberi@users.noreply.github.com>
2025-06-08 18:16:36 -0700

    Update finetune.sh

--- scripts/finetune.sh
+++ scripts/finetune.sh
@@ -10,17 +10,17 @@ CUDA_VISIBLE_DEVICES=0 python src/finetune.py \
     --per_device_train_batch_size 4 \
 
 
-# CUDA_VISIBLE_DEVICES=0 python src/finetune.py \
-#     --model_name Llama-3.2-1B-Instruct \
-#     --dataset TOFU_QA_retain99_ft \
-#     --trainer_config configs/trainer/trainer_config.yaml \
-#     --output_dir outputs/Llama-3.2-1B-Instruct_TOFU_QA_retain99 \
-#     --per_device_train_batch_size 4 \
+CUDA_VISIBLE_DEVICES=0 python src/finetune.py \
+    --model_name Llama-3.2-1B-Instruct \
+    --dataset TOFU_QA_retain99_ft \
+    --trainer_config configs/trainer/trainer_config.yaml \
+    --output_dir outputs/Llama-3.2-1B-Instruct_TOFU_QA_retain99 \
+    --per_device_train_batch_size 4 \
 
 
-# CUDA_VISIBLE_DEVICES=0 python src/finetune.py \
-#     --model_name Llama-3.2-1B-Instruct \
-#     --dataset TOFU_QA_full \
-#     --trainer_config configs/trainer/trainer_config.yaml \
-#     --output_dir outputs/Llama-3.2-1B-Instruct_TOFU_QA_full \
-#     --per_device_train_batch_size 4 \
+CUDA_VISIBLE_DEVICES=0 python src/finetune.py \
+    --model_name Llama-3.2-1B-Instruct \
+    --dataset TOFU_QA_full \
+    --trainer_config configs/trainer/trainer_config.yaml \
+    --output_dir outputs/Llama-3.2-1B-Instruct_TOFU_QA_full \
+    --per_device_train_batch_size 4 \
